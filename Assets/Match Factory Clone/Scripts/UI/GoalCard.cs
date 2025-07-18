ff9a5167c845594ad04c4e27ef20d80a521295b4
Lymite <lymite00@gmail.com>
2025-07-18 20:12:48 +0300

    Icons added and goal cards improved

--- Assets/Match Factory Clone/Scripts/UI/GoalCard.cs
+++ Assets/Match Factory Clone/Scripts/UI/GoalCard.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +8,20 @@ public class GoalCard : MonoBehaviour
     [Header("Elements")] 
     [SerializeField] private TextMeshProUGUI amountText;
     [SerializeField] private Image iconImage;
+    
+    [SerializeField] private GameObject checkMark;
+    [SerializeField] private GameObject backFace;
+    [SerializeField] private Animator animator;
 
+    private void Start()
+    {
+        animator.enabled = false;
+    }
+
+    private void Update()
+    {
+        backFace.SetActive(Vector3.Dot(Vector3.forward, transform.forward) < 0);
+    }
 
     public void Configure(int initialAmount, Sprite icon)
     {
@@ -18,10 +32,27 @@ public class GoalCard : MonoBehaviour
     public void UpdateAmount(int amount)
     {
         amountText.text = amount.ToString();
+
+        Bump();
+    }
+
+    private void Bump()
+    {
+        LeanTween.cancel(gameObject);
+
+        transform.localScale = Vector3.one;
+        LeanTween.scale(gameObject, Vector3.one * 1.1f,.2f).setLoopPingPong(1);
     }
 
     public void Complete()
     {
-        gameObject.SetActive(false);
+        animator.enabled = true;
+
+        checkMark.SetActive(true);
+        amountText.gameObject.SetActive(false);
+        
+        animator.Play("Complete");
+        //amountText.text = "";
+        //gameObject.SetActive(false);
     }
 }
